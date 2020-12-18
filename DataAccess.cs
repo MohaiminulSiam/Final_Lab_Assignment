68d81b80710014bf6c888a4192f02fb312fa78c5
MohaiminulSiam <msiam1996@gmail.com>
2020-12-18 22:48:39 +0600

    DataAccess Class Created

---
+++ DataAccess.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+namespace Final_Lab_Assignment
+{
+    public class DataAccess
+    {
+        private SqlConnection sqlcon;
+        public SqlConnection Sqlcon
+        {
+            get { return sqlcon; }
+            set { sqlcon = value; }
+        }
+
+        private SqlCommand sqlcom;
+        public SqlCommand Sqlcom
+        {
+            get { return sqlcom; }
+            set { sqlcom = value; }
+        }
+
+        private SqlDataAdapter sda;
+        public SqlDataAdapter Sda
+        {
+            get { return sda; }
+            set { sda = value; }
+        }
+
+        private DataSet ds;
+        public DataSet Ds
+        {
+            get { return ds; }
+            set { ds = value; }
+        }
+
+        //internal DataTable dt;
+
+        public DataAccess()
+        {
+            this.Sqlcon = new SqlConnection(@"Data Source=DESKTOP-32IK7D6;Initial Catalog=final_cSharp;User ID=sa;Password=fahim");
+            Sqlcon.Open();
+        }
+
+        private void QueryText(string query)
+        {
+            this.Sqlcom = new SqlCommand(query, this.Sqlcon);
+        }
+
+        public DataSet ExecuteQuery(string sql)
+        {
+            this.QueryText(sql);
+            this.Sda = new SqlDataAdapter(this.Sqlcom);
+            this.Ds = new DataSet();
+            this.Sda.Fill(this.Ds);
+            return Ds;
+        }
+
+        public int ExecuteUpdateQuery(string sql)
+        {
+            this.QueryText(sql);
+            int u = this.Sqlcom.ExecuteNonQuery();
+            return u;
+        }
+    }
+}
--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Final_Lab_Assignment_code")]
+[assembly: AssemblyTitle("Final_Lab_Assignment")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Final_Lab_Assignment_code")]
+[assembly: AssemblyProduct("Final_Lab_Assignment")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
